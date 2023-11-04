b587255176fcb4ea815451b2a1e783a6f48d4eb2
Ssuamje <side2850@naver.com>
2023-11-04 16:00:01 +0900

    원상복구

--- src/main/java/lotto/lotto/Lotto.java
+++ src/main/java/lotto/lotto/Lotto.java
@@ -1,17 +1,30 @@
 package lotto.lotto;
 
-import lotto.Validator;
-
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+	private static final int LOTTO_SIZE = 6;
+	private static final int LOTTO_MIN_NUM = 1;
+	private static final int LOTTO_MAX_NUM = 45;
 	private final List<Integer> numbers;
 
 	public Lotto(List<Integer> numbers) {
-		this.numbers = numbers;
-		Validator.throwIfInvalidConstruction(this);
+		validate(numbers);
+		this.numbers = Collections.unmodifiableList(numbers);
 	}
 
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != LOTTO_SIZE) {
+			throw new IllegalArgumentException("로또 번호의 개수는 6개 이어야 합니다.");
+		}
+		if (numbers.stream().anyMatch(number -> number < LOTTO_MIN_NUM || number > LOTTO_MAX_NUM)) {
+			throw new IllegalArgumentException("로또 번호는 1 ~ 45 사이의 숫자여야 합니다.");
+		}
+		if (numbers.stream().anyMatch(number -> Collections.frequency(numbers, number) > 1)) {
+			throw new IllegalArgumentException("로또 번호에 중복된 숫자가 있습니다.");
+		}
+	}
 
 	@Override
 	public String toString() {
